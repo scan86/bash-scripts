96dd52a889ec9759e66286dc9e6d2d19c8dfe00b
Dmitriy Anikin <d.anikin@creditnet.ru>
2015-01-21 15:13:22 +0300

    template and backup pgsql

---
+++ bash_template.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -o nounset
+
+# global vars #
+
+logfile=""
+
+function log() {
+ local msg dt
+
+ msg=$@
+ dt=$(date "+%F %H:%M:%S")
+ 
+ if [ ! -z $logfile ]
+ then
+	 echo $dt : $msg >> $logfile
+ else
+	 echo $dt : $msg
+ fi
+}
+
+function print_help() {
+ echo "Help block here..."
+ exit 1
+}
+
+while [ $# -ne 0 ]
+do
+	case "$1" in
+		--key)
+			value=$2
+			shift; shift;
+		;;
+		*)
+			print_help
+			exit 1
+		;;
+
+	esac
+done
+
+
